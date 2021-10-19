76412c783d5241d21b3ae4b49e5ffc7f6e208541
Martin Lachev <martin.lachev@flowscapesolutions.com>
2021-10-19 18:30:56 +0300

    Initial Commit

---
+++ BreakingBadCharacters/BreakingBadCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadCharactersApp.swift
+//  BreakingBadCharacters
+//
+//  Created by Cognitven on 19.10.21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
